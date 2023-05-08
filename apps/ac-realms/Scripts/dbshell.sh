6e9400de53444c523c6c58d2d707f2be9b032964
Pourman <apourman@protonmail.com>
2023-05-07 19:15:29 -0700

    build(ac-realms): 📦️ add latest prefix to scripts

--- apps/ac-realms/Scripts/dbshell.sh
+++ apps/ac-realms/Scripts/dbshell.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-docker exec -it ace-db bash -c "mysql -u acedockeruser -p2020acEmulator2017";
+docker exec -it ace-db-latest bash -c "mysql -u acedockeruser -p2020acEmulator2017";
