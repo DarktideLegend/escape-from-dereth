eade1ebc310b1746304c9293aa6851378ff22066
Pourman <apourman@protonmail.com>
2023-06-02 17:08:09 -0700

    feat(efd-server): ✨ add ability to open/close whitelested towns to players

--- apps/efd-server/Source/ACE.Server/EscapeFromDereth/Towns/TownManager.cs
+++ apps/efd-server/Source/ACE.Server/EscapeFromDereth/Towns/TownManager.cs
@@ -1,9 +1,11 @@
 using ACE.Common;
 using ACE.Entity;
+using ACE.Server.Entity;
 using ACE.Server.EscapeFromDereth.Common;
 using ACE.Server.WorldObjects;
 using log4net;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
@@ -12,33 +14,85 @@ using System.Threading.Tasks;
 
 namespace ACE.Server.EscapeFromDereth.Towns
 {
+    // This should probably be moved to ACE.Server/Managers
     public class TownManager
     {
         private static readonly ILog log = LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
 
-        private static ImmutableList<Town> Towns = ImmutableList.Create(
-            new Town("Arwic", EFDHelpers.slocToPosition("0xC6A90013 [51.633736 68.552765 42.005001] -0.080479 0.000000 0.000000 0.996756 0")), // Arwic
-            new Town("Rithwic", EFDHelpers.slocToPosition("0xC98C0028 [113.666000 190.259003 22.009998] -0.707107 0.000000 0.000000 -0.707107 0")), // Rithwic
-            new Town("Eastham", EFDHelpers.slocToPosition("0xCE950023 [115.072716 68.856300 20.004999] 0.860280 0.000000 0.000000 -0.509823 0"))); // Eastham
+        public readonly static ImmutableDictionary<ushort, Town> Towns = new Dictionary<ushort, Town>()
+        {
+            { 0xC6A9, new Town("Arwic",
+                EFDHelpers.slocToPosition("0xC6A90013 [51.633736 68.552765 42.005001] -0.080479 0.000000 0.000000 0.996756 0"),
+                EFDHelpers.slocToPosition("0xC5A80004 [22.075813 93.041489 56.005001] 0.464588 0.000000 0.000000 0.885527 393216")) },
+            { 0xC98C, new Town("Rithwic",
+                EFDHelpers.slocToPosition("0xC98C0028 [113.666000 190.259003 22.009998] -0.707107 0.000000 0.000000 -0.707107 0"),
+                EFDHelpers.slocToPosition("0xCA8C002D [132.680344 106.881424 12.004999] 0.884355 0.000000 0.000000 0.466815 393216")) },
+            { 0xCE95, new Town("Eastham",
+                EFDHelpers.slocToPosition("0xCE950023 [115.072716 68.856300 20.004999] 0.860280 0.000000 0.000000 -0.509823 0"),
+                EFDHelpers.slocToPosition("0xCE960004 [3.525760 93.337540 19.711185] -0.259322 0.000000 0.000000 0.965791 393216")) }
+        }.ToImmutableDictionary();
 
+        private readonly static ImmutableList<Town> TownsList = Towns.ToList().Select((kvp) => kvp.Value).ToImmutableList();
+
+        private static HashSet<Town> ClosedTowns = new HashSet<Town>();
+
+        // This should probably be moved to ACE.Entity
         public class Town
         {
-            public readonly string name;
-            public readonly Position position;
-            public Town(string name, Position position) {
+            public string name { get; private set; }
+            public Position position { get; private set; }
+            public bool isClosed { get; private set; } = false;
+            public Position bootLocation { get; private set; }
+
+            public Town(string name, Position position, Position bootLocation)
+            {
                 this.name = name;
                 this.position = position;
+                this.bootLocation = bootLocation;
+            }
+
+            internal void close()
+            {
+                isClosed = true;
+            }
+
+            internal void open()
+            {
+                isClosed = false;
             }
         }
 
+        public bool closeTown(Town town)
+        {
+            if (ClosedTowns.Contains(town))
+                return false;
+
+            ClosedTowns.Add(town);
+            town.close();
+            return true;
+        }
+
+
+        public bool openTown(Town town)
+        {
+            if (ClosedTowns.TryGetValue(town, out var closedTown))
+            {
+                closedTown.open();
+                return ClosedTowns.Remove(town);
+            }
+
+            return false;
+        }
+
+
         public static Town GetRandomTown()
         {
-            return Towns[ThreadSafeRandom.Next(0, Towns.Count - 1)];
+            return TownsList[ThreadSafeRandom.Next(0, TownsList.Count - 1)];
         }
 
         public static Town GetClosestTownFromWorldObject(WorldObject wo)
         {
-            return Towns.Aggregate((previous, next) =>
+            return TownsList.Aggregate((previous, next) =>
             {
                 if (previous.position.DistanceTo(wo.Location) <= next.position.DistanceTo(wo.Location))
                     return previous;
