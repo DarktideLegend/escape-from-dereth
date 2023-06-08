11145498082f3ebec413ad9ac11db1d6f256b633
Pourman <apourman@protonmail.com>
2023-06-07 19:05:19 -0700

    fix(efd-server): 🐛 handle RecallsDisabled properly

--- apps/efd-server/Source/ACE.Server/WorldObjects/Player_Properties.cs
+++ apps/efd-server/Source/ACE.Server/WorldObjects/Player_Properties.cs
@@ -1255,7 +1255,7 @@ namespace ACE.Server.WorldObjects
 
         public bool RecallsDisabled
         {
-            get => RealmRuleset.GetProperty(RealmPropertyBool.HasRecalls);
+            get => !RealmRuleset.GetProperty(RealmPropertyBool.HasRecalls);
         }
 
         public AetheriaBitfield AetheriaFlags
