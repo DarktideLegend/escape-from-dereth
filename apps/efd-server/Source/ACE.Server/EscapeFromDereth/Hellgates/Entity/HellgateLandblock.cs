f84aa15ef319bd293f79b9be62d8607ef8cebdfc
Pourman <apourman@protonmail.com>
2023-06-22 22:44:17 -0700

    feat(efd-server): ✨ add HellgateLandblock entity

---
+++ apps/efd-server/Source/ACE.Server/EscapeFromDereth/Hellgates/Entity/HellgateLandblock.cs
@@ -0,0 +1,21 @@
+using ACE.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACE.Server.EscapeFromDereth.Hellgates.Entity
+{
+    internal class HellgateLandblock
+    {
+        public Position DropLocation;
+        public string Name;
+
+        public HellgateLandblock(Position dropLocation, string name) 
+        {
+            this.DropLocation = dropLocation;
+            this.Name = name;   
+        }
+    }
+}
