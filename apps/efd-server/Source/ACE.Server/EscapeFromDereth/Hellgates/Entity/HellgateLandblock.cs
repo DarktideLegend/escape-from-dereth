8efddeb4f9b44baf53f1411390b2b8787bce0c11
Pourman <apourman@protonmail.com>
2023-06-29 04:30:27 -0700

    feat(efd-server): ✨ add BossLocation and ExitLocation to hellgates

--- apps/efd-server/Source/ACE.Server/EscapeFromDereth/Hellgates/Entity/HellgateLandblock.cs
+++ apps/efd-server/Source/ACE.Server/EscapeFromDereth/Hellgates/Entity/HellgateLandblock.cs
@@ -10,11 +10,15 @@ namespace ACE.Server.EscapeFromDereth.Hellgates.Entity
     public class HellgateLandblock
     {
         public Position DropLocation;
+        public Position ExitLocation;
+        public Position BossLocation;
         public string Name;
 
-        public HellgateLandblock(Position dropLocation, string name) 
+        public HellgateLandblock(Position dropLocation, Position exitLocation, Position bossLocation, string name) 
         {
             this.DropLocation = dropLocation;
+            this.ExitLocation = exitLocation;
+            this.BossLocation = bossLocation;
             this.Name = name;   
         }
     }
