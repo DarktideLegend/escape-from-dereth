a73271cf9f7a432f589f894b24ea40673f69b51d
Pourman <apourman@protonmail.com>
2023-07-26 14:32:09 -0700

    refactor(efd-server): ♻️ add HellgateRepository

---
+++ apps/efd-server/Source/ACE.Server/EscapeFromDereth/Hellgates/Data/HellgateJsonDto.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACE.Server.EscapeFromDereth.Hellgates.Data
+{
+    internal class HellgateJsonDto
+    {
+        public string Name { get; set; }
+        public string DropLocation { get; set; }    
+        public string ExitLocation { get; set; }    
+        public string BossLocation { get; set; }
+    }
+}
+
+
+
